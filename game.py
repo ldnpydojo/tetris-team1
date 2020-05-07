f01cf21034a502e3545669ce6c5e6cbb04c22fed
Tom Viner <tom.viner@sohonet.com>
2020-05-07 21:10:27 +0100

    add movement

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import board
+import time
 
-b = board.Board((10, 20))
+
 
 square = '''\
 XX
@@ -17,5 +18,25 @@ def to_coords(shape: str):
 
 shapes = {square,}
 
+
+def main():
+    b = board.Board((10, 20))
+
+    for i in range(30):
+        b2 = b.copy(with_data=True)
+
+        for coord in to_coords(square):
+            x, y = coord
+            new_coord = x, y + i
+
+            if b[new_coord]:
+                # piece stops here
+            b2[new_coord] = '■ '
+
+        print('\n' * 10)
+        b2.draw(use_borders=False)
+        time.sleep(0.5)
+
+
 if __name__ == '__main__':
-    print(b.draw())
+    main()
